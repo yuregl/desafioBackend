af0ae655e2817a3683766935e79a84d7623ff3d9
yuregl <yuri.galdino@vendaecia.com>
2021-10-01 19:30:16 -0300

    chore: adiçao de migrations de Product

---
+++ src/database/migrations/1632950649935-CreateTableProducts.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateTableProdutos1632877580626 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'nameProduct',
+            type: 'varchar',
+          },
+          {
+            name: 'ImageUri',
+            type: 'varchar',
+          },
+          {
+            name: 'priceProduct',
+            type: 'float',
+          },
+          {
+            name: 'quantity',
+            type: 'int',
+          },
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true
+          }
+        ] 
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
