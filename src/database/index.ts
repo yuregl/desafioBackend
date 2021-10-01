20f2d12f8f3608b43fde80fb3bb56670fa2a8e9b
yuregl <yuri.galdino@vendaecia.com>
2021-10-01 19:29:37 -0300

    feat: criado conexao com o banco

---
+++ src/database/index.ts
@@ -0,0 +1,27 @@
+import { ConnectionOptions, createConnection } from 'typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const options = {
+  name: 'default',
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: [process.env.TYPEORM_ENTITIES],
+  migrations: [process.env.TYPEORM_MIGRATIONS],
+  cli: {
+    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+  },
+  logging: process.env.TYPEORM_LOGGING === 'true',
+} as ConnectionOptions;
+
+
+
+export const startDatabase = async (): Promise<void> => {
+  console.log('Starting database...');
+  await createConnection(options);
+  console.log('Database started!');
+};
