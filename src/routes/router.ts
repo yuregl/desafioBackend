ebed8af16294632973bab869b30434cdb5200ad6
yuregl <yuri.galdino@vendaecia.com>
2021-10-02 20:02:35 -0300

    feat: injeçao de rotas de user no express

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import 'express-async-errors';
 
 import { createProductsRouter } from './ProductsRoutes';
+import { createUsersRoutes } from './UsersRoutes';
 
 async function exportApp(){
   const app = express(); 
@@ -10,6 +12,19 @@ async function exportApp(){
   app.use(cors());
   app.use(express.json());
   app.use(createProductsRouter());
+  app.use(createUsersRoutes())
+  app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+      return response.status(400).json({
+        error: err.message
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+    })
+  });
   return app;
 }
 
