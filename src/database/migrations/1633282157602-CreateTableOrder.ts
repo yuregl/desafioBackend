f6fb13266ff86bce0ff622edb05871b8130e7a03
yuregl <yuri.galdino@vendaecia.com>
2021-10-03 17:24:18 -0300

    feat: Criada migration de order

---
+++ src/database/migrations/1633282157602-CreateTableOrder.ts
@@ -0,0 +1,53 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTableOrder1633282157602 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+            isGenerated: true
+          },
+          {
+            name: 'note',
+            type: 'varchar'
+          },
+          {
+            name: "user_id",
+            type: "int"
+          },
+          {
+            name: "price_total",
+            type: "float"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          }
+
+        ],
+        foreignKeys: [
+          {
+            name: "FKUserOrder",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete:"CASCADE"
+          }
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('orders')
+  }
+
+}
