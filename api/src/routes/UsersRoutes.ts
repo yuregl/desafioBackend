1fc9933bd51ebc0ac1b2d355ffe65f0377a3d7f6
yuregl <yuri.galdino@vendaecia.com>
2021-10-04 23:18:58 -0300

    feat: adiçao de validaçao
    adicionado validaçao do middleware nas rotas de user

--- api/src/routes/UsersRoutes.ts
+++ api/src/routes/UsersRoutes.ts
@@ -3,6 +3,9 @@ import { getCustomRepository } from 'typeorm';
 import { UserController } from '../controllers/UserController';
 import { UsersService } from '../services/UsersService';
 import { UsersRepositories } from '../repositories/UsersRepositories';
+import validation from '../middleware/Validation';
+import { ValidatorUserCreate, ValidatorUserLogin } from '../Validators';
+
 
 const routesUser = Router();
 
@@ -11,8 +14,8 @@ function createUsersRoutes(){
   const usersService = new UsersService(usersRepositories);
   const usersController = new UserController(usersService);
 
-  routesUser.post('/user/new', usersController.handleCreateUser);
-  routesUser.post('/login', usersController.handleLogin);
+  routesUser.post('/user/new', validation(ValidatorUserCreate) ,usersController.handleCreateUser);
+  routesUser.post('/login', validation(ValidatorUserLogin), usersController.handleLogin);
 
   return routesUser;
 }
