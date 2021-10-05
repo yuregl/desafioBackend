3f69cf977a4a7079cf66d2d85b229b14f8e106f8
yuregl <yuri.galdino@vendaecia.com>
2021-10-05 20:25:53 -0300

    styles: ajuste de linhas

--- api/src/util/saveImage.ts
+++ api/src/util/saveImage.ts
@@ -13,19 +13,15 @@ interface IStorage{
 }
 
 export default function storage(): IStorage {
-
   const storage = multer.diskStorage({
     destination: tmpFolder,
+    
     filename(request, file, callback) {
-
       if (!fs.existsSync(tmpFolder)) {
         fs.mkdirSync(tmpFolder, { recursive: true });
       }
       const nameImageProduct = Date.now() + '-' + Math.round(Math.random() * 1E9);
       const value = `${nameImageProduct}`;
-
-      console.log('value: ',value);
-      
       request.destination = `${tmpFolder}/${value}`;
       callback(null, value);
     }
