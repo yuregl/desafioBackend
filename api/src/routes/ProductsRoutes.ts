7b7fb9fd4f8a42618b50f41cfd0957560b069810
yuregl <yuri.galdino@vendaecia.com>
2021-10-04 23:20:57 -0300

    feat: adicionado validaçao
    adicionado validaçao nas rotas de Products

--- api/src/routes/ProductsRoutes.ts
+++ api/src/routes/ProductsRoutes.ts
@@ -5,11 +5,10 @@ import { ProductsRepositories } from '../repositories/ProductsRepositories';
 import { ProductsService } from '../services/ProductsService';
 import multer from 'multer';
 import verifyAuth from '../util/AuthMiddlewae';
-
-
+import validation from '../middleware/Validation';
+import { ValidatorProductCreate } from '../Validators';
 
 import saveImage from '../util/saveImage';
-import { verify } from 'jsonwebtoken';
 
 const routesProducts = Router();
 
@@ -19,8 +18,9 @@ function createProductsRouter(){
   const productsController = new ProductController(productsService);
 
   routesProducts.get('/products', verifyAuth ,productsController.handleListProducts);
-  routesProducts.post('/products', 
+  routesProducts.post('/products/new',
     multer(saveImage()).single('media'),
+    validation(ValidatorProductCreate) ,
     productsController.handleCreateProduct);
 
   return routesProducts;
