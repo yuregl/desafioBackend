39f1c2f8421e6240f6b4acdfb47c14f84d891484
yuregl <yuri.galdino@vendaecia.com>
2021-10-06 12:53:23 -0300

    refactor: ajuste para manutençao do projeto

--- cafexyz/src/App.tsx
+++ cafexyz/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { CreateAccount } from "./pages/CreateAccount";
+
 
 function App() {
   return (
-    <h1>Hello world</h1>
+
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/account/new" component={CreateAccount} />
+    </BrowserRouter>
   );
 }
 
